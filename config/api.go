0571c3aab165f3f63b24e01459ea89c3529215b6
Reljod Oreta <reljod.oreta@factset.com>
2021-05-05 23:32:10 +0800

    enh: add config for swagger doc

---
+++ config/api.go
@@ -0,0 +1,14 @@
+package config
+
+const (
+	Title        = "Reljod API Swagger Example"
+	Version      = "1.0"
+	Description  = "This is Reljod Collection of APIs."
+	ContactName  = "Reljod T. Oreta"
+	ContactUrl   = "https://www.linkedin.com/in/reljodoreta/"
+	ContactEmail = "oretareljod@gmail.com"
+	LicenseName  = "Apache 2.0"
+	LicenseUrl   = "http://www.apache.org/licenses/LICENSE-2.0.html"
+	Host         = "localhost:8888"
+	BasePath     = "/api/v1"
+)
